e93e4e0e37b42d2a2dd5a2e98285c2fabf28b59a
avivfox93 <avivfox93@gmail.com>
2020-02-01 16:57:55 +0200

    updated

--- api/post.js
+++ api/post.js
@@ -14,7 +14,7 @@ const createPost = async(req,res)=>{
 const getPosts = async(req,res)=>{
     try{
         const user = res.locals.user;
-        console.log('user: ' + user);
+        console.log('user: ' + user.friendsId);
         user.friendsId.push(user._id);
         const posts = await Post.find().limit(50).where('userProfile').in(user.friendsId)
             .where('date').lte(req.body.date).limit(50)
