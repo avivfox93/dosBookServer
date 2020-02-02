a058275754bd7179f29da1ed0f880bf4de49a774
avivfox93 <avivfox93@gmail.com>
2020-02-03 00:48:13 +0200

    updated

--- api/comment.js
+++ api/comment.js
@@ -1,5 +1,6 @@
 
 const User = require('../entities/User');
+const Post = require('../entities/Post');
 const Comment = require('../entities/Comment');
 
 const postComment = async(req,res)=>{
@@ -9,6 +10,9 @@ const postComment = async(req,res)=>{
         c.userProfile = res.locals.user._id;
         const comment = new Comment(p);
         await comment.save();
+        const post = Post.findById(req.body.post_id);
+        post.comments.push(comment._id);
+        await post.save();
         res.send({id:comment._id});
     }catch(error){
         console.error(error);
