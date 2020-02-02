671576ad9ed8582de83fe73f0bf2d429aa53098e
avivfox93 <avivfox93@gmail.com>
2020-02-02 03:11:58 +0200

    updated

--- api/post.js
+++ api/post.js
@@ -4,7 +4,7 @@ const Comment = require('../entities/Comment');
 
 const createPost = async(req,res)=>{
     try{
-        req.body.post.userProfile = res.locals.user._id;
+        req.body.post.userProfile = res.locals.user;
         const post = new Post(req.body.post);
         await post.save();
         res.send({id:post._id});
